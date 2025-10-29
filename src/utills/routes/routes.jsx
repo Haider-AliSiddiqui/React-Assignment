e90724dfd4a495ec76b975ee4fd02e1888525b0a
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2025-10-29 11:07:10 +0500

    Change default route to Counter component

--- src/utills/routes/routes.jsx
+++ src/utills/routes/routes.jsx
@@ -7,7 +7,7 @@ import ColorChanger from "../../Pages/colorChanger/colorChanger";
 const AppRoutes = () => {
     return(
             <Routes> 
-                <Route path='/counter' element={<Counter/>}/>
+                <Route path='/' element={<Counter/>}/>
                 <Route path='/ourMenu' element={<OurMenu/>}/>
                 <Route path='/colorChanger' element={<ColorChanger/>}/>
             </Routes> 
