7e308d00444e56e37d9e9b9642c0c76fbfc37d9f
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2025-11-09 12:51:57 +0500

    Fix all import capitalization for Vercel

--- src/utills/routes/routes.jsx
+++ src/utills/routes/routes.jsx
@@ -1,16 +1,17 @@
 // All routes are defined here
-import {Routes, Route} from "react-router-dom";
-import Counter from '../../Pages/counter/counter';
-import ourmenu from "../../Pages/ourmenu/OurMenu";
-import ColorChanger from "../../Pages/colorChanger/colorChanger";
+import { Routes, Route } from "react-router-dom";
+import Counter from '../../Pages/counter/Counter';
+import OurMenu from "../../Pages/ourmenu/OurMenu";
+import ColorChanger from "../../Pages/colorChanger/ColorChanger";
 
 const AppRoutes = () => {
-    return(
-            <Routes> 
-                <Route path='/' element={<Counter/>}/>
-                <Route path='/ourmenu' element={<ourmenu/>}/>
-                <Route path='/colorChanger' element={<ColorChanger/>}/>
-            </Routes> 
-    )
-}
+    return (
+        <Routes> 
+            <Route path='/' element={<Counter />} />
+            <Route path='/ourmenu' element={<OurMenu />} />
+            <Route path='/colorChanger' element={<ColorChanger />} />
+        </Routes> 
+    );
+};
+
 export default AppRoutes;
