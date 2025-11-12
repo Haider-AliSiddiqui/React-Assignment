589d9f2258746595cebec3bbb33e260369cde59f
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2025-11-12 13:02:08 +0500

    Fix import case for ColorChanger component

--- src/utills/routes/routes.jsx
+++ src/utills/routes/routes.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route } from "react-router-dom";
 import CounterApp from "../../Pages/CounterApp/CounterApp";
 import OurMenu from "../../Pages/ourMenu/OurMenu";
-import ColorChanger from "../../Pages/ColorChanger/ColorChanger";
+import ColorChanger from "../../Pages/colorChanger/ColorChanger";
 
 const AppRoutes = () => {
     return (
