8949f1b0881722bcbb3402776a38fa6f34572d0e
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2025-11-12 12:57:17 +0500

    Fix import path for OurMenu component

--- src/utills/routes/routes.jsx
+++ src/utills/routes/routes.jsx
@@ -1,7 +1,7 @@
 // All routes are defined here
 import { Routes, Route } from "react-router-dom";
 import CounterApp from "../../Pages/CounterApp/CounterApp";
-import OurMenu from "../../Pages/OurMenu/OurMenu";
+import OurMenu from "../../Pages/ourMenu/OurMenu";
 import ColorChanger from "../../Pages/ColorChanger/ColorChanger";
 
 const AppRoutes = () => {
